5bcf5f8fc20f212cde9ab7918980ed84dc862aad
solom <solom@berserker>
2019-10-01 17:15:27 -0400

    Implemented INSERT algorithms

--- src/gcms/rdms/InsertUtility.java
+++ src/gcms/rdms/InsertUtility.java
@@ -83,7 +83,7 @@ public class InsertUtility {
             for (int i = 0; i < selectedRowCount; i++) {
                 // get the value in the Day, Job, and EmpID columns at the selected row index
                 dayValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 0).toString());
-                jobValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 1).toString());
+                jobValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 1));
                 emplID = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 2).toString());
                 
                 // prepare the values for insertion and execute the query
@@ -101,6 +101,12 @@ public class InsertUtility {
         Connection.disconnect();
     } // end insertToEmplSchedule
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the Employees table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToEmployees(JTable formTable) {
         
     }
@@ -124,7 +130,7 @@ public class InsertUtility {
         try (PreparedStatement pstmt = Connection.getConnection().prepareStatement(sql)) {
             for (int i = 0; i < selectedRowCount; i++) {
                 // get the value in the Job column at the selected row index
-                jobValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 0).toString());
+                jobValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 0));
                 
                 // prepare the value for insertion and execute the query
                 pstmt.setString(1, jobValue);
@@ -139,29 +145,220 @@ public class InsertUtility {
         Connection.disconnect();
     } // end insertToJobList
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the Members table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToMembers(JTable formTable) {
         
     }
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the Purchase table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToPurchase(JTable formTable) {
+        DefaultTableModel tableModel = (DefaultTableModel) formTable.getModel();
+        int selectedRowCount = formTable.getSelectedRowCount();
+        int[] selectedRowIndices = formTable.getSelectedRows();
+        int transNumValue, dayValue, memIDValue;
+        String lNameValue, fNameValue;
+        double totalAmountValue;
+        String sql = "INSERT INTO Purchase(TransNum, Day, MemberID, LName, FName, TotalAmt) VALUES(?, ?, ?, ?, ?, ?)";
         
-    }
+        // connect to the database
+        Connection.connect(CONNECTION_STR);
+        
+        try (PreparedStatement pstmt = Connection.getConnection().prepareStatement(sql)) {
+            for (int i = 0; i < selectedRowCount; i++) {
+                /* get the values in the TransNum, Day, MemberID, LName, FName,
+                and TotalAmt columns at the selected row index */
+                transNumValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 0).toString());
+                dayValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 1).toString());
+                memIDValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 2).toString());
+                lNameValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 3));
+                fNameValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 4));
+                totalAmountValue = Double.parseDouble(tableModel.getValueAt(selectedRowIndices[i], 5).toString());
+                
+                // prepare the values for insertion and execute the query
+                pstmt.setInt(1, transNumValue);
+                pstmt.setInt(2, dayValue);
+                pstmt.setInt(3, memIDValue);
+                pstmt.setString(4, lNameValue);
+                pstmt.setString(5, fNameValue);
+                pstmt.setDouble(6, totalAmountValue);
+                pstmt.executeUpdate();
+            } // end for
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(new JFrame(), CREATE_ERROR_MSG,
+                "Dialog", JOptionPane.ERROR_MESSAGE);
+        } // end try-catch
+        
+        // disconnect from the database
+        Connection.disconnect();
+    } // end insertToPurchase
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the PurchaseLine table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToPurchaseLine(JTable formTable) {
+        DefaultTableModel tableModel = (DefaultTableModel) formTable.getModel();
+        int selectedRowCount = formTable.getSelectedRowCount();
+        int[] selectedRowIndices = formTable.getSelectedRows();
+        int transNumValue, purchLineNumValue;
+        String itemValue;
+        int quantityValue;
+        String sql = "INSERT INTO PurchaseLine(TransNum, PurchLineNum, Item, Qty) VALUES(?, ?, ?, ?)";
         
-    }
+        // connect to the database
+        Connection.connect(CONNECTION_STR);
+        
+        try (PreparedStatement pstmt = Connection.getConnection().prepareStatement(sql)) {
+            /* get the values in the TransNum, PurchaseLineNum, Item, and Qty
+            columns at the selected row index */
+            for (int i = 0; i < selectedRowCount; i++) {
+                transNumValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 0).toString());
+                purchLineNumValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 1).toString());
+                itemValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 2));
+                quantityValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 3).toString());
+                
+                // prepare the values for insertion and execute the query
+                pstmt.setInt(1, transNumValue);
+                pstmt.setInt(2, purchLineNumValue);
+                pstmt.setString(3, itemValue);
+                pstmt.setInt(4, quantityValue);
+                pstmt.executeUpdate();
+            } // end for
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(new JFrame(), CREATE_ERROR_MSG,
+                "Dialog", JOptionPane.ERROR_MESSAGE);
+        } // end try-catch
+        
+        // disconnect from the database
+        Connection.disconnect();
+    } // end insertToPurchaseLine
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the Rates table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToRates(JTable formTable) {
+        DefaultTableModel tableModel = (DefaultTableModel) formTable.getModel();
+        int selectedRowCount = formTable.getSelectedRowCount();
+        int[] selectedRowIndices = formTable.getSelectedRows();
+        String itemValue;
+        double priceValue;
+        String sql = "INSERT INTO Rates(Item, Price) VALUES(?, ?)";
         
-    }
+        // connect to the database
+        Connection.connect(CONNECTION_STR);
+        
+        try (PreparedStatement pstmt = Connection.getConnection().prepareStatement(sql)) {
+            for (int i = 0; i < selectedRowCount; i++) {
+                // get the values in the Item and Price columns at the selected row index
+                itemValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 0));
+                priceValue = Double.valueOf(tableModel.getValueAt(selectedRowIndices[i], 1).toString());
+                
+                // prepare the values for insertion and execute the query
+                pstmt.setString(1, itemValue);
+                pstmt.setDouble(2, priceValue);
+                pstmt.executeUpdate();
+            } // end for
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(new JFrame(), CREATE_ERROR_MSG,
+                "Dialog", JOptionPane.ERROR_MESSAGE);
+        } // end try-catch
+        
+        // disconnect from the database
+        Connection.disconnect();
+    } // end insertToRates
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the TeeSchedule table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToTeeSchedule(JTable formTable) {
+        DefaultTableModel tableModel = (DefaultTableModel) formTable.getModel();
+        int selectedRowCount = formTable.getSelectedRowCount();
+        int[] selectedRowIndices = formTable.getSelectedRows();
+        int dayValue, timeValue, memIDValue;
+        String lNameValue, fNameValue;
+        String sql = "INSERT INTO TeeSchedule(Day, Time, MemberID, LName, FName) VALUES(?, ?, ?, ?, ?)";
         
-    }
+        // connect to the database
+        Connection.connect(CONNECTION_STR);
+        
+        try (PreparedStatement pstmt = Connection.getConnection().prepareStatement(sql)) {
+            for (int i = 0; i < selectedRowCount; i++) {
+                /* get the values in the Day, Time, MemberID, LName, and FName
+                columns at the selected row indices */
+                dayValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 0).toString());
+                timeValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 1).toString());
+                memIDValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 2).toString());
+                lNameValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 3));
+                fNameValue = String.valueOf(tableModel.getValueAt(selectedRowIndices[i], 4));
+                
+                // prepare the values for insertion and execute the query
+                pstmt.setInt(1, dayValue);
+                pstmt.setInt(2, timeValue);
+                pstmt.setInt(3, memIDValue);
+                pstmt.setString(4, lNameValue);
+                pstmt.setString(5, fNameValue);
+                pstmt.executeUpdate();
+            } // end for
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(new JFrame(), CREATE_ERROR_MSG,
+                "Dialog", JOptionPane.ERROR_MESSAGE);
+        } // end try-catch
+        
+        // disconnect from the database
+        Connection.disconnect();
+    } // end insertToTeeSchedule
     
+    /**
+     * Operation INSERT Calendar of the GCMS's CRUD design. Connects to the
+     * internal database, selects the TeeTimes table, and inserts the specified
+     * rows into the database table.
+     * @param formTable the form's table with selected records to be inserted
+     */
     public static void insertToTeeTimes(JTable formTable) {
+        DefaultTableModel tableModel = (DefaultTableModel) formTable.getModel();
+        int selectedRowCount = formTable.getSelectedRowCount();
+        int[] selectedRowIndices = formTable.getSelectedRows();
+        int timeValue;
+        String sql = "INSERT INTO TeeTimes(Time) VALUES(?)";
         
-    }
+        // connect to the database
+        Connection.connect(CONNECTION_STR);
+        
+        try (PreparedStatement pstmt = Connection.getConnection().prepareStatement(sql)) {
+            for (int i = 0; i < selectedRowCount; i++) {
+                // get the values in the Time column at the selected row index
+                timeValue = Integer.parseInt(tableModel.getValueAt(selectedRowIndices[i], 0).toString());
+                
+                // prepare the value for insertion and execute the query
+                pstmt.setInt(1, timeValue);
+                pstmt.executeUpdate();
+            } // end for
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(new JFrame(), CREATE_ERROR_MSG,
+                "Dialog", JOptionPane.ERROR_MESSAGE);
+        } // end try-catch
+        
+        // disconnect from the database
+        Connection.disconnect();
+    } // end insertToTeeTimes
     
     private static final String CONNECTION_STR =
         "jdbc:sqlite:data/gcms_db.db";
